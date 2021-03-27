9ff8ab6f682246ba5d2d838213a58fe6f561b8e9
Blazej Dolicki <blazej.dol@gmail.com>
2021-03-27 18:12:45 +0100

    Change run_multiple.py to compare noise accuracy between algorithms

--- run_multiple.py
+++ run_multiple.py
@@ -6,22 +6,23 @@ now = datetime.now()
 date_time = now.strftime("%d_%m_%Y_%H_%M_%S")
 log_file = open(f"logs/log_{date_time}.txt", "w")
 
-tasks = ["standard", "img_clas", "target_clas"]
+tasks = ["standard"] #, "img_clas", "target_clas"]
 n_imgs = [2]
 same_class_probs = [0.5]
-seeds = [7]
-test_sets = ["Cifar100","gaussian_noise"]
+seeds = [7, 122, 809]
+game_types = ["SenderReceiverRnnGS", "SenderReceiverRnnReinforce"]
 for task in tasks:
     for num_imgs in n_imgs:
         for same_class_prob in same_class_probs:
             for seed in seeds:
-                for test_set in test_sets:
+                for game_type in game_types:
                     subprocess.call(["python", "main.py",  
                                     "--task", task, 
                                     "--num_imgs", str(num_imgs), 
                                     "--same_class_prob", str(same_class_prob),
                                     "--seed",str(seed),
-                                    "--testdataset  ",test_set], 
+                                    "--game_type", game_type, 
+                                    "--eval_noise"], 
                                     stdout=log_file)
             
 with open(f"logs/log_{date_time}.txt", "r") as f:
